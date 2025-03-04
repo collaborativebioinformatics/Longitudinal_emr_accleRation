bd0cf87713ec3ff1bc59de93809456bfc31596a2
samblechman <seb203@pitt.edu>
2025-03-04 12:49:48 -0500

    Added user input script and output

---
+++ example_user_input/GenerateExampleUserInput.R
@@ -0,0 +1,9 @@
+
+library(yaml)
+
+writeLines(as.yaml(list('filterTables' = list('Diagnosis' = list('long_title' = 'Pneumonia')),
+                        'select' = list('Labs'),
+                        'outcome' = list('Mortality'))),
+           '~/Desktop/Longitudinal_emr_accleRation/example_user_input/user_input_yaml.txt')
+           
+           
