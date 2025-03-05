45805b66d59b64cd65dde1f0363e75ac56e635d1
Glenn Ross-Dolan <149796430+GlennRDx@users.noreply.github.com>
2025-03-05 19:44:38 +0000

    Update run_tetrad_from_yaml.r

--- Scripts/run_tetrad_from_yaml.r
+++ Scripts/run_tetrad_from_yaml.r
@@ -1,8 +1,11 @@
 # Load necessary libraries
 library(yaml)
 
+# Accept the YAML file as a command-line argument
+args <- commandArgs(trailingOnly = TRUE)
+yaml_file <- args[1]  # Get the input YAML file
+
 # Read the YAML file
-yaml_file <- "user_input_yaml.txt"
 yaml_data <- yaml.load_file(yaml_file)
 
 # Extract options from the YAML file
@@ -10,28 +13,52 @@ algorithm <- yaml_data$tetrad_args$algorithm
 alpha <- yaml_data$tetrad_args$alpha
 score <- yaml_data$tetrad_args$score
 data_type <- yaml_data$tetrad_args$datatype  # Assuming 'datatype' is included in the YAML
-dataset <- yaml_data$tetrad_args$dataset
 knowledge <- yaml_data$tetrad_args$knowledge
 test <- yaml_data$tetrad_args$test
 numberResampling <- yaml_data$tetrad_args$numberResampling
 delimiter <- yaml_data$tetrad_args$delimiter
 prefix <- yaml_data$tetrad_args$prefix
 
-# Construct the tetrad_command
-tetrad_command <- paste0("java -jar ./causal-cmd-1.12.0-jar-with-dependencies.jar",
-                         " --algorithm ", algorithm,
-                         " --data-type ", data_type,
-                         " --dataset ", dataset,
-                         " --delimiter ", delimiter,
-                         # " --metadata ", paste0(getwd(), "/metadata_v01.txt"),
-                         " --test ", test,
-                         " --alpha ", alpha,
-                         " --score ", score,
-                         " --numberResampling ", numberResampling,
-                         " --prefix ", prefix)
+# Get the list of CSV files in the directory (the directory where ParseUserInput.R placed them)
+csv_files <- list.files(pattern = "*.csv")
 
-# Print the command for debugging purposes
-print(tetrad_command)
+# Loop over the CSV files
+for (dataset in csv_files) {
+  # Load the dataset
+  dataset_df <- read.csv(dataset, header = TRUE, sep = ',')
 
-# Execute the command
-system(command = tetrad_command)
+  # Remove columns with more than 30% NA values
+  na_threshold <- 0.30
+  dataset_df <- dataset_df[, colSums(is.na(dataset_df)) / nrow(dataset_df) <= na_threshold]
+
+  # Impute remaining NA values with column means
+  dataset_df[] <- lapply(dataset_df, function(x) {
+    if (is.numeric(x)) {
+      # Impute with column mean
+      x[is.na(x)] <- mean(x, na.rm = TRUE)
+    }
+    return(x)
+  })
+
+  # Save the cleaned dataset
+  cleaned_dataset <- paste0("cleaned_", dataset)
+  write.csv(dataset_df, cleaned_dataset, row.names = FALSE)
+
+  # Construct the tetrad_command
+  tetrad_command <- paste0("java -jar ./causal-cmd-1.12.0-jar-with-dependencies.jar",
+                           " --algorithm ", algorithm,
+                           " --data-type ", data_type,
+                           " --dataset ", cleaned_dataset,
+                           " --delimiter ", delimiter,
+                           " --test ", test,
+                           " --alpha ", alpha,
+                           " --score ", score,
+                           " --numberResampling ", numberResampling,
+                           " --prefix ", prefix)
+
+  # Print the command for debugging purposes
+  print(tetrad_command)
+
+  # Execute the command
+  system(command = tetrad_command)
+}
