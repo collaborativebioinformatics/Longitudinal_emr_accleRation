2fa40e7b0fb5a5d0037da682c4400168c57ad289
npcooley <npcooley@gmail.com>
2025-03-05 14:19:07 -0500

    bash script to turn mimic into a sqlite db

---
+++ Scripts/Create_SQLite_DB.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+
+# originally written for dnanexus use, where bash is in /usr/bin
+# and not /bin like on my system ...
+# if sqlite3 is not present
+# apt-get sqlite or whatever your environment package manager is
+
+arg1="MIMIC"
+arg2='.csv'
+arg3="mimic3_v3.sqlite"
+
+targets=$(find "${arg1}" -type f -iname "*${arg2}" | sed 's/^\.\///g' | sort)
+# echo ${targets}
+
+for m1 in ${targets}; do
+  echo "${m1}"
+  m2=${m1%.csv}
+  m3=${m2##*/}
+  sqlite3 -csv "${arg3}" ".import ${m1} ${m3}"
+done
+
+echo "done!"
